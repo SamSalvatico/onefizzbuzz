86e115fc99bf38fae42fd1eb381732813a9e6865
Sam Salvatico <samuele.salvatico@cws.it>
2020-12-12 20:42:48 +0100

    Added the fizzBuzz script and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# onefizzbuzz
+# onefizzbuzz - Samuele Salvatico
+
+This code was developed on Ubuntu 20.04 and tested using PHP 7.2.32.
+
+Assuming that PHP is installed on your PC, to run the script you have to open the terminal into the script folder and run
+```
+php -f oneFizzBuzz.php
+```
+
+Et voilà! Here there are the FizzBuzzed values!
+
+### Hint
+The default max value to fizz buzz is 20, if you want to set your own you can run
+
+```
+php -f oneFizzBuzz.php yourOwnIntegereValue
+```
+
+E.g.
+
+```
+php -f oneFizzBuzz.php 10
+```
---
+++ oneFizzBuzz.php
@@ -0,0 +1,49 @@
+<?php
+
+/** @var string FIZZ */
+const FIZZ = 'Fizz';
+
+/** @var string BUZZ */
+const BUZZ = 'Buzz';
+
+/** @var int $theMaxValue */
+$theMaxValue = (empty($argv) || !isset($argv[1]) || !is_numeric($argv[1]))
+    ? 20 : intval($argv[1]);
+
+fizzBuzzAndPrintValues(1, $theMaxValue);
+
+/**
+ * Print the fizzBuzzedValues
+ * 
+ * @param int $minValue The min value to fizzBuzz
+ * @param int $maxValue The max value to fizzBuzz
+ */
+function fizzBuzzAndPrintValues(int $minValue, int $maxValue)
+{
+    for ($i = $minValue; $i <= $maxValue; $i++) {
+        echo (fizzBuzzAValue($i) . PHP_EOL);
+    }
+}
+
+/**
+ * Return the processed fizzBuzzAValue for an integer
+ * @param int $theValue the integer to process
+ * @return int|string the FizzBuzzedValue
+ */
+function fizzBuzzAValue(int $theValue)
+{
+    /** @var int|string $outputVal */
+    $outputVal = '';
+
+    if ($theValue % 3 === 0) {
+        $outputVal = FIZZ;
+    }
+    if ($theValue % 5 === 0) {
+        $outputVal .= BUZZ;
+    }
+    if (empty($outputVal)) {
+        return $theValue;
+    } else {
+        return $outputVal;
+    }
+}
